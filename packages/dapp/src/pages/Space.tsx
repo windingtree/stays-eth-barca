68b91be7478901bc7bafbe116dcba47bb01a7337
bobetbat <b.bobetayev.97@gmail.com>
2022-06-16 18:07:12 +0200

    style: booking result popups

--- packages/dapp/src/pages/Space.tsx
+++ packages/dapp/src/pages/Space.tsx
@@ -1,5 +1,5 @@
 import { PageWrapper } from './PageWrapper';
-import { Box, Text, Image, Carousel, Spinner, Anchor } from 'grommet';
+import { Box, Text, Image, Carousel, Spinner, Anchor, Card, CardHeader, CardBody, CardFooter } from 'grommet';
 import { useAppState } from '../store';
 import { useMemo, useEffect, useState, useCallback } from 'react';
 import { BookWithDai } from '../components/buttons/BookWithDai';
@@ -9,12 +9,14 @@ import { ExternalLink } from '../components/ExternalLink';
 import { getNetwork } from '../config';
 import { centerEllipsis } from '../utils/strings';
 import { useContract } from '../hooks/useContract';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import { utils, BigNumber as BN } from 'ethers';
 //import { Header } from './MyTokens';
-// import { CustomText, Title } from '../components/StayVoucherQr';
+// import { Text, Title } from '../components/StayVoucherQr';
 import styled from 'styled-components';
 import Logger from '../utils/logger';
+import { getDate } from '../utils/dates';
+import { FormCheckmark } from 'grommet-icons';
 
 // Initialize logger
 const logger = Logger('Space');
@@ -37,6 +39,43 @@ export const Contact = styled(Anchor)`
   font-size: 12px;
   line-height: 24px;
   text-align: center;
+`;
+const GreenCheckmark = styled(FormCheckmark)`
+  border-radius: 50%;
+  background: #47A180;
+  color: white;
+`;
+
+const RedCheckmark = styled(FormCheckmark)`
+  border-radius: 50%;
+  background: white;
+`;
+
+export const CustomText = styled(Text)`
+  color: #0D0E0F;
+  // font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const CustomBoldText = styled(Text)`
+  color: #0D0E0F;
+  // font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+
+const CustomTitle = styled(Text)`
+  color: #0D0E0F;
+  font-weight: 500;
+  font-size: 34px;
+  line-height: 40px;
+  // margin-bottom: .5rem;
 `;
 
 export const Space: React.FC = () => {
@@ -178,7 +217,7 @@ export const Space: React.FC = () => {
         </Box>
       </MessageBox>
 
-      <MessageBox type='info' show={!!tokenId}>
+      {/* <MessageBox type='info' show={!!tokenId}>
         <Box direction='row'>
           <Box>
             Booked successfully!
@@ -186,7 +225,72 @@ export const Space: React.FC = () => {
             <NavLink to='https://discord.gg/kZYnMncsg4'> Join our discord</NavLink>
           </Box>
         </Box>
-      </MessageBox>
+      </MessageBox> */}
+
+      {!!tokenId ? <Card round={false}>
+        <CardHeader border={{ color: '#47A180' }} background='#CBF7DC' align='center' justify='center' pad='0.75rem'>
+          <GreenCheckmark color='white' />
+          <Text color='#47A180'>
+            Booking Successful
+          </Text>
+        </CardHeader>
+        <CardBody border={{ color: '#999EAB', side: "vertical" }} pad='1rem' direction='row' align='center' justify='around'>
+          <Box>
+            <CustomTitle>{facility?.name}</CustomTitle>
+            <CustomText>{facility?.address.streetAddress + ', ' + facility?.address.locality}</CustomText>
+            <CustomText>{getDate(searchParams?.startDay ?? 0).toFormat('MM.dd.yy')}-{getDate(Number(searchParams?.startDay ?? 0) + Number(numberDays)).toFormat('MM.dd.yy')}</CustomText>
+            <CustomText>{facility?.type}, {numberDays} {numberDays === 1 ? 'room' : 'rooms'}</CustomText>
+            <CustomText>{getPrice(numberDays, roomsNumber)} xDAI</CustomText>
+            <CustomBoldText weight={800}> Token Id#{tokenId}</CustomBoldText>
+          </Box>
+          <Box>
+            <Image
+              height='250'
+              width='250'
+              style={{ borderRadius: '50%' }}
+              src={facility?.media.logo}
+            />
+          </Box>
+        </CardBody>
+        <CardFooter border={{ color: '#999EAB', side: "bottom" }} />
+      </Card> : null}
+
+      {!!tokenId ? <Box pad='small'>
+        <CustomText textAlign='center'>Make sure to save your token ID# so that you can use it to check-in at the hotel, if you go to the “My Tokens” section you can also download the QR code.</CustomText>
+      </Box> : null}
+
+      {!!error ? <Card round={false}>
+        <CardHeader border={{ color: '#47A180' }} background='#DB717A' align='center' justify='center' pad='0.75rem'>
+          <RedCheckmark color='#47A180' />
+          <Text color='white'>
+            Booking Failed
+          </Text>
+        </CardHeader>
+        <CardBody border={{ color: '#999EAB', side: "vertical" }} pad='1rem' direction='row' align='center' justify='around'>
+          <Box>
+            <CustomTitle>{facility?.name}</CustomTitle>
+            <CustomText>{facility?.address.streetAddress + ', ' + facility?.address.locality}</CustomText>
+            <CustomText>{getDate(searchParams?.startDay ?? 0).toFormat('MM.dd.yy')}-{getDate(Number(searchParams?.startDay ?? 0) + Number(numberDays)).toFormat('MM.dd.yy')}</CustomText>
+            <CustomText>{facility?.type}, {numberDays} {numberDays === 1 ? 'room' : 'rooms'}</CustomText>
+            <CustomText>{getPrice(numberDays, roomsNumber)} xDAI</CustomText>
+          </Box>
+          <Box>
+            <Image
+              height='250'
+              width='250'
+              style={{ borderRadius: '50%' }}
+              src={facility?.media.logo}
+            />
+          </Box>
+        </CardBody>
+        <CardFooter border={{ color: '#999EAB', side: "bottom" }}>
+          {error}
+        </CardFooter>
+      </Card> : null}
+
+      {!!error ? <Box pad='small'>
+        <CustomText textAlign='center'>In the future, you can see the details of the reservation in your personal account section "Bookings"</CustomText>
+      </Box> : null}
 
       {isLoading && !space && <Box> No space with given id </Box>}
       {isLoading && !!space && !tokenId &&
@@ -205,41 +309,41 @@ export const Space: React.FC = () => {
             {space.name}
           </Text>
           {!!facility &&
-              <Box align='center' justify='between' direction='row' style={{ fontSize: '1em' }} margin= 'medium'>
-                <Anchor
-                  size='small'
-                  label={facility.address.streetAddress + ', ' + facility.address.locality}
-                  href={'https://www.openstreetmap.org/search?query=' + encodeURI(facility.address.streetAddress + ', ' + facility.address.locality)}
-                  target="_blank"
-                  style={{ fontSize: '1em', lineHeight: '1.4em', color: 'black' }}
-                />
-                &nbsp;&nbsp;&nbsp;
-                <Anchor
-                  size='small'
-                  label={facility?.contact?.website ?? ''}
-                  href={facility?.contact?.website}
-                  title={facility?.name}
-                  target="_blank"
-                  style={{ fontSize: '1em', lineHeight: '1.4em', color: 'black' }}
-                />
-                &nbsp;&nbsp;&nbsp;
-                <Anchor
-                  size='large'
-                  label={facility.contact?.email}
-                  href={`mailto:${facility.contact?.email}`}
-                  title={facility?.name}
-                  target="_blank"
-                  style={{ fontSize: '1em', lineHeight: '1.4em', color: 'black' }}
-                />
-                <Anchor
-                  size='large'
-                  label={facility.contact?.phone}
-                  href={`tel:${facility.contact?.phone}`}
-                  title={facility?.name}
-                  target="_blank"
-                  style={{ fontSize: '1em', lineHeight: '1.4em', marginLeft:'0.25rem', color: 'black' }}
-                />
-              </Box>
+            <Box align='center' justify='between' direction='row' style={{ fontSize: '1em' }} margin='medium'>
+              <Anchor
+                size='small'
+                label={facility.address.streetAddress + ', ' + facility.address.locality}
+                href={'https://www.openstreetmap.org/search?query=' + encodeURI(facility.address.streetAddress + ', ' + facility.address.locality)}
+                target="_blank"
+                style={{ fontSize: '1em', lineHeight: '1.4em', color: 'black' }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <Anchor
+                size='small'
+                label={facility?.contact?.website ?? ''}
+                href={facility?.contact?.website}
+                title={facility?.name}
+                target="_blank"
+                style={{ fontSize: '1em', lineHeight: '1.4em', color: 'black' }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <Anchor
+                size='large'
+                label={facility.contact?.email}
+                href={`mailto:${facility.contact?.email}`}
+                title={facility?.name}
+                target="_blank"
+                style={{ fontSize: '1em', lineHeight: '1.4em', color: 'black' }}
+              />
+              <Anchor
+                size='large'
+                label={facility.contact?.phone}
+                href={`tel:${facility.contact?.phone}`}
+                title={facility?.name}
+                target="_blank"
+                style={{ fontSize: '1em', lineHeight: '1.4em', marginLeft: '0.25rem', color: 'black' }}
+              />
+            </Box>
           }
 
           <Box
